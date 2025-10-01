5fa5c3f2ee1f79eece5945c4cd7d5cbcd7c80a20
Anne Risgaard Hostrup <annerishos@gmail.com>
2025-10-01 13:17:46 +0200

    Add Treatment as List and invidually

---
+++ PetCare/Controllers/TreatmentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCareAPI.Models;
+
+namespace PetCare.Controllers;
+
+[Route("treatments")]
+public class TreatmentController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:5000/treatments");
+        ViewBag.Treatments = await response.Content.ReadAsAsync<Treatment[]>();
+        return View();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Single(int id)
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync($"http://localhost:5000/treatments/{id}");
+        if (!response.IsSuccessStatusCode) return NotFound();
+        ViewBag.Treatment = await response.Content.ReadAsAsync<Treatment>();
+        return View("Index");
+    }
+}
