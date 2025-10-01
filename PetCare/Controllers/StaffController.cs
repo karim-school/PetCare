f5a4ab422fa12b20abb8bf4e42987451e0948fe7
Anne Risgaard Hostrup <annerishos@gmail.com>
2025-10-01 12:57:14 +0200

    Add Staff as list and invidually

---
+++ PetCare/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCareAPI.Models;
+
+namespace PetCare.Controllers;
+
+[Route("staff")]
+
+public class StaffController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:5000/staff");
+        ViewBag.Staffs = await response.Content.ReadAsAsync<Staff[]>();
+        return View();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Single(int id)
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync($"http://localhost:5000/staff/{id}");
+        if (!response.IsSuccessStatusCode) return NotFound();
+        ViewBag.Staff = await response.Content.ReadAsAsync<Staff>();
+        return View("Index");
+    }
+}
