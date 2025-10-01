17b85676b5bf92c1368ac860546d3719dbd2db13
Karim <72211@edu.sde.dk>
2025-10-01 13:06:07 +0200

    feat: add medication controller and views

---
+++ PetCare/Controllers/MedicationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCareAPI.Models;
+
+namespace PetCare.Controllers;
+
+[Route("medications")]
+public class MedicationController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:5000/medications");
+        ViewBag.Medications = await response.Content.ReadAsAsync<Medication[]>();
+        return View();
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Single(int id)
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync($"http://localhost:5000/medications/{id}");
+        ViewBag.Medication = await response.Content.ReadAsAsync<Medication>();
+        return View("Index");
+    }
+}
