8d0221ad4e7b78911dbf9e319ac609f61a8bd8cf
Karim <72211@edu.sde.dk>
2025-10-01 12:41:18 +0200

    feat: add pet controller and views

---
+++ PetCare/Controllers/PetsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCareAPI.Models;
+
+namespace PetCare.Controllers;
+
+[Route("pets")]
+public class PetsController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:5000/pets");
+        ViewBag.Pets = await response.Content.ReadAsAsync<Pet[]>();
+        return View();
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Single(int id)
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync($"http://localhost:5000/pets/{id}");
+        ViewBag.Pet = await response.Content.ReadAsAsync<Pet>();
+        return View("Index");
+    }
+}
