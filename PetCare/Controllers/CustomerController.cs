c4c533da842a433ffdef87493089f6c890684bbe
Ruru <renehinrichsen@yahoo.dk>
2025-10-01 12:52:28 +0200

    Added Customer list and individual customer

---
+++ PetCare/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetCareAPI.Models;
+
+namespace PetCare.Controllers
+{
+    [Route("customers")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:5000/customers");
+            ViewBag.Customers = await response.Content.ReadAsAsync<Customer[]>();
+            return View();
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Single(int id)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync($"http://localhost:5000/customers/{id}");
+            if (!response.IsSuccessStatusCode) return NotFound();
+            ViewBag.Customer = await response.Content.ReadAsAsync<Customer>();
+            return View("Index");
+        }
+    }
+}
