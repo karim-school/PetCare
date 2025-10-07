efecd28725b25377552596c8abbbd35063b66ddb
Karim <72211@edu.sde.dk>
2025-10-07 11:18:27 +0200

    feat: add new connection strings with limited permissions

--- PetCareAPI/Program.cs
+++ PetCareAPI/Program.cs
@@ -30,41 +30,40 @@ public class Program
 
         app.UseAuthorization();
 
-        var admin = new PetCareContext("admin");
-        // var read = new PetCareContext("read");
-        // var write = new PetCareContext("write");
-        // var exec = new PetCareContext("exec");
+        // var admin = new PetCareContext("admin");
+        var read = new PetCareContext("read");
+        var exec = new PetCareContext("exec");
 
-        MapResource("/appointments",admin.Appointments
+        MapResource("/appointments", read.Appointments
             .Include(a => a.PetView)
             .Include(a => a.Staff)
             .Include(a => a.Treatment),
             (int arg1) => appointment => appointment.Id == arg1);
-        MapResource("/customers", admin.Customers
+        MapResource("/customers", read.Customers
             .Include(customer => customer.ZipCode),
             (int arg1) => customer => customer.Id == arg1);
-        MapResource("/invoices", admin.Invoices
+        MapResource("/invoices", read.Invoices
             .Include(invoice => invoice.Customer)
             .Include(invoice => invoice.Appointment),
             (int arg1) => invoice => invoice.Id == arg1);
-        MapResource("/medications", admin.Medications, (int arg1) => medication => medication.Id == arg1);
-        MapResource("/pets", admin.PetsView, (int arg1) => pet => pet.Id == arg1);
-        MapResource("/species", admin.Species, (char arg1) => species => species.Id == arg1);
-        MapResource("/staff", admin.Staff, (int arg1) => staff => staff.Id == arg1);
-        MapResource("/treatments", admin.Treatments, (int arg1) => treatment => treatment.Id == arg1);
-        MapResource("/zipcodes", admin.ZipCodes, (string arg1) => zipCode => zipCode.Code == arg1);
+        MapResource("/medications", read.Medications, (int arg1) => medication => medication.Id == arg1);
+        MapResource("/pets", read.PetsView, (int arg1) => pet => pet.Id == arg1);
+        MapResource("/species", read.Species, (char arg1) => species => species.Id == arg1);
+        MapResource("/staff", read.Staff, (int arg1) => staff => staff.Id == arg1);
+        MapResource("/treatments", read.Treatments, (int arg1) => treatment => treatment.Id == arg1);
+        MapResource("/zipcodes", read.ZipCodes, (string arg1) => zipCode => zipCode.Code == arg1);
         app.MapGet("/medication_by_species/{arg1}",
-            Select(admin.SpeciesMedications, (char arg1) => speciesMedication => speciesMedication.SpeciesId == arg1));
+            Select(read.SpeciesMedications, (char arg1) => speciesMedication => speciesMedication.SpeciesId == arg1));
         app.MapGet("/species_by_medication/{arg1:int}",
-            Select(admin.SpeciesMedications, (int arg1) => speciesMedication => speciesMedication.MedicationId == arg1));
+            Select(read.SpeciesMedications, (int arg1) => speciesMedication => speciesMedication.MedicationId == arg1));
         app.MapGet("/treatment_by_species/{arg1}",
-            Select(admin.SpeciesTreatment, (char arg1) => speciesTreatment => speciesTreatment.SpeciesId == arg1));
+            Select(read.SpeciesTreatment, (char arg1) => speciesTreatment => speciesTreatment.SpeciesId == arg1));
         app.MapGet("/species_by_treatment/{arg1:int}",
-            Select(admin.SpeciesTreatment, (char arg1) => speciesTreatment => speciesTreatment.TreatmentId == arg1));
+            Select(read.SpeciesTreatment, (char arg1) => speciesTreatment => speciesTreatment.TreatmentId == arg1));
         app.MapGet("/customers/{arg1:int}/pets", async (HttpContext _, int arg1) =>
         {
-            var ids = await admin.Pets.Where(pet => pet.CustomerId == arg1).Select(pet => pet.Id).ToArrayAsync();
-            var pets = await admin.PetsView.Where(pet => ids.Contains(pet.Id)).ToArrayAsync();
+            var ids = await read.Pets.Where(pet => pet.CustomerId == arg1).Select(pet => pet.Id).ToArrayAsync();
+            var pets = await read.PetsView.Where(pet => ids.Contains(pet.Id)).ToArrayAsync();
             return Results.Ok(pets);
         });
         app.MapPost("/pets/new", async context =>
@@ -77,7 +76,7 @@ public class Program
             var breed = pet.Breed;
             var birthDate = pet.BirthDate;
             var sex = pet.Sex;
-            var results = await admin.Database.SqlQuery<decimal>($"dbo.sp_new_pet {customer}, {name}, {species}, {breed}, {birthDate}, {sex}").ToArrayAsync();
+            var results = await exec.Database.SqlQuery<decimal>($"dbo.sp_new_pet {customer}, {name}, {species}, {breed}, {birthDate}, {sex}").ToArrayAsync();
             var id = (int) results.FirstOrDefault();
             var response = new NewPetForm.Response()
             {
